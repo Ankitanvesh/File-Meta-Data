e6a857e40b0e88ad8ee66e24cfcd852cd23ef8cc
Ankit Anvesh <ankit192812@rediffmail.com>
2018-07-03 21:29:47 +0530

    commiting file meta data

---
+++ server.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var express = require('express');
+var cors = require('cors');
+const bodyParser = require('body-parser');
+// require and use "multer"...
+const multer = require('multer');
+const upload = multer();
+var app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
+
+app.post('/api/fileanalyse',upload.single('upfile'),(req,res) => {
+ //res.json(req.file);
+  res.json({
+  'name' :  req.file.originalname ,
+  'type' :  req.file.mimetype ,
+  'size' :  req.file.size
+  });
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
